b5242830700b014f4725683075cd7e0d76b50441
Debjit <devjeet.official77@gmail.com>
2023-08-12 14:34:53 +0530

    Linked List

---
+++ Linked List/BasicLinked.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void insertAtHead(Node *&head, Node *&tail, int data)
+{
+    // check for emplty LL
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    // check for emplty LL
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    Node *newNode = new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 30);
+    insertAtHead(head, tail, 40);
+    insertAtHead(head, tail, 50);
+    insertAtHead(head, tail, 60);
+    insertAtTail(head, tail, 77);
+    insertAtTail(head, tail, 78);
+    insertAtTail(head, tail, 79);
+    print(head);
+    return 0;
+}
